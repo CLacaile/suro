da3b862cff257694e31bf7e9c441c7c48c368e28
CLacaile <clementlacaile@gmail.com>
2023-08-10 21:24:03 +0200

    moved questions management to a proper custom hook

--- suro-frontend/src/components/molecules/QuestionList/QuestionList.js
+++ suro-frontend/src/components/molecules/QuestionList/QuestionList.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from "react";
 import Question from "../Question/Question";
-import { fetchRandomQuestions } from "../../../lib/questions";
+import { useQuestions } from "../../../lib/questions";
 import { getRandomColor } from "../../../lib/colors";
 
 export default function QuestionList() {
   const [bgColor, setBgColor] = useState();
-  const [questions, setQuestions] = useState([]);
+  const {questions, addQuestions} = useQuestions();
 
   useEffect(() => {
-    const generateFirstTwoQuestions = async () => {
-      const response = await fetchRandomQuestions(2);
-      setQuestions(response);
-    }
-    if (!questions.length) generateFirstTwoQuestions();
     if (!bgColor) setBgColor(getRandomColor());
   }, []);
 
   const onNextQuestion = async () => {
-    const newQuestions = await fetchRandomQuestions();
-    setQuestions((prevQuestions) => [...prevQuestions, ...newQuestions]);
+    //const newQuestions = await fetchRandomQuestions();
+    await addQuestions();
+   //setQuestions((prevQuestions) => [...prevQuestions, ...newQuestions]);
     const randomColor = getRandomColor(bgColor);
     setBgColor(randomColor);
   };
--- suro-frontend/src/lib/questions.js
+++ suro-frontend/src/lib/questions.js
@@ -1,6 +1,32 @@
-export const fetchRandomQuestions = async (nbQuestionsToFetch = 1) => {
-  // eslint-disable-next-line no-undef
-  const response = await fetch(process.env.REACT_APP_SURO_API_URL + '/questions/random?size=' + nbQuestionsToFetch);
+import { useEffect, useState } from "react";
+
+const fetchRandomQuestions = async (nbQuestionsToFetch = 1) => {
+  const response = await fetch(
+    // eslint-disable-next-line no-undef
+    process.env.REACT_APP_SURO_API_URL +
+      "/questions/random?size=" +
+      nbQuestionsToFetch
+  );
   const randomQuestions = await response.json();
   return randomQuestions;
-}
+};
+
+export const useQuestions = () => {
+  const [questions, setQuestions] = useState([]);
+
+  const addQuestions = async (qty = 1) => {
+    const randomQuestions = await fetchRandomQuestions(qty);
+    setQuestions((prevQuestions) => [...prevQuestions, ...randomQuestions]);
+  };
+
+  useEffect(() => {
+    const fetchInitialQuestions = async () => {
+      const randomQuestions = await fetchRandomQuestions(2);
+      setQuestions(randomQuestions);
+    };
+
+    if (!questions.length) fetchInitialQuestions();
+  }, []);
+
+  return { questions, addQuestions };
+};
