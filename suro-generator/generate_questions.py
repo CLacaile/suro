db6c409bbaa3a467b7e3ca66ae02aec8b40fa065
CLacaile <clementlacaile@gmail.com>
2023-12-10 18:26:54 +0100

    refactoring the generator so it is easier to use and can generate more questions

--- suro-generator/generate_questions.py
+++ suro-generator/generate_questions.py
@@ -1,8 +1,9 @@
-from openai import OpenAI
+#from openai import OpenAI
 from datetime import datetime
 import json
+import argparse
 
-client = OpenAI()
+#client = OpenAI()
 
 SYSTEM_CONTEXT = """Tu es un assitant concu pour générer des questions de 100 caractères maximum 
 et 4 réponses courtes (30 car. maximum) par question (dont la première est la bonne réponse). 
@@ -45,10 +46,48 @@ def generate_questions(nb_questions, themes, show_cost=True):
 
     return response.choices[0].message.content
 
-output_json = generate_questions(1000, ['histoire', 'géographie', 'sciences', 'musique', 'sport', 'arts', 'littérature'])
 
-# Création du nom du fichier avec la date et l'heure actuelles
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-output_filename = f"output_questions_{timestamp}.json"
-with open(output_filename, "w") as json_file:
-    json_file.write(output_json)
+def get_args():
+    parser = argparse.ArgumentParser(description='Génération de questions via chatgpt')
+    parser.add_argument('-t', '--themes', type=str, help='Nom du fichier txt contenant les themes', required=True)
+    return parser.parse_args()
+
+def get_themes(file_name):
+    lines = []
+
+    with open(file_name, 'r', encoding='utf-8') as file:
+        for line in file:
+            lines.append(line.strip())  # strip() pour supprimer les caractères de saut de ligne
+
+    print("Themes à générer : " + ', '.join(lines))
+    return lines
+
+
+def print_questions(output_json):
+    # Création du nom du fichier avec la date et l'heure actuelles
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_filename = f"output_questions_{timestamp}.json"
+    with open(output_filename, "w") as json_file:
+        json_file.write(output_json)
+
+
+def main():
+    print("==== SURO GENERATOR ====")
+
+    # Récupération des arguments
+    args = get_args()
+    themes_filename = args.themes
+
+    # Parcours des thèmes pour générer les questions 
+    themes = get_themes(themes_filename)
+    for theme in themes:
+      print("Génération des questions du theme : " + theme)
+      print(theme)
+
+    # output_json = generate_questions(1000, themes)
+    output_json = "{}"
+    print_questions(output_json)
+
+        
+if __name__ == '__main__':
+    main()
