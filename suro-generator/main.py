b935db1db74da1062ad64f099e9b64cc1e6fa047
Clément LACAÏLE <clementlacaile@gmail.com>
2024-01-03 20:41:59 +0000

    extracted nb of questoins and batch size as arguments

--- suro-generator/main.py
+++ suro-generator/main.py
@@ -7,7 +7,11 @@ def get_args():
     parser = argparse.ArgumentParser(
         description='Génération de questions via chatgpt')
     parser.add_argument('-t', '--themes', type=str,
-                        help='Nom du fichier txt contenant les themes', required=True)
+                        help='Nom du fichier txt contenant les thèmes', required=True)
+    parser.add_argument('-n', '--nb_questions', type=int,
+                        help='Nombre de questions à générer par thème', required=True)
+    parser.add_argument('-s', '--batch_size', type=int,
+                        help='Nombre de questions dans un lot', required=False, default=10)
     return parser.parse_args()
 
 
@@ -16,7 +20,6 @@ def get_themes(file_name):
 
     with open(file_name, 'r', encoding='utf-8') as file:
         for line in file:
-            # strip() pour supprimer les caractères de saut de ligne
             lines.append(line.strip())
 
     print("Themes à générer : " + ', '.join(lines))
@@ -28,13 +31,15 @@ def main():
 
     args = get_args()
 
-    # Récupération des themes
+    # Récupération des arguments
     themes_filename = args.themes
     themes = get_themes(themes_filename)
+    nb_questions_per_theme = args.nb_questions
+    nb_questions_per_batch = args.batch_size
 
     # Generation des questions
     suro_generator = SuroGenerator(
-        themes=themes, nb_questions_per_theme=50, nb_questions_per_batch=10)
+        themes, nb_questions_per_theme, nb_questions_per_batch)
     suro_generator.generate_questions()
     suro_generator.show_total_cost()
 
